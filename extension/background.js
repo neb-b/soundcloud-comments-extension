cad45d4e35a487789a8b773b6c1b825bed64ed12
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-05-28 14:05:36 -0700

    better html structure

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,9 @@
-// Can't use content script matches because of Soundcloud's SPAness
-// If url match, load content script
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  var bkg = chrome.extension.getBackgroundPage();
-  // bkg.console.log('foo');
+// Allow content script access to soundcloud
 
+
+// Can't use content script matches because of Soundcloud's SPAness
+// If url match, send message to start updatePage function
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   const url = tab.url
   const regex = /(?:(?:http|https):\/\/)?(?:www.)?soundcloud.com\/(?:[\w\-]*\/[\w\-]*)/
   if (url.match(regex)) {
@@ -11,6 +11,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     const firstLevel = pieces[3]
 
     if (firstLevel !== 'you' && firstLevel !== 'charts') {
+      console.log('\n\n\n yep\n\n\n');
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {url})
       })
--- extension/script.js
+++ extension/script.js
@@ -1,43 +1,129 @@
 'use strict'
 
-let hasUrl = null
+SC.initialize({
+  client_id: "02d54efe6695649f5c72cae57841e44a"
+})
 
+let activeUrl = null
 chrome.runtime.onMessage.addListener(function({url}) {
-  if (!hasUrl && url) {
-    hasUrl = true
+  if (!activeUrl || url !== activeUrl) {
+    activeUrl = url
     updatePage(url)
   }
 })
 
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+
+
 function updatePage(url) {
-  function fetchComments() {
-    fetch(`https://hackernews-server-jvudyvzoto.now.sh?url=${url}`)
-      .then(res => res.json())
-      .then(comments => {
-        // do something with the comments
-      })
-      .catch(err => console.log('err', err))
+  function createComment(commentData) {
+
+    /*
+    <li commentContainer>
+      <span>{comment.timestamp}</span>
+      <div>
+        <div>{comment.user.userName}</div>
+        <div>{comment.body}</div>
+      </div>
+    </li>
+    */
+
+    const commentContainer = document.createElement('li')
+    commentContainer.className = 'ce--comment'
+
+    const timeEl = document.createElement('div')
+    timeEl.innerHTML = millisToMinutesAndSeconds(commentData.timestamp)
+    timeEl.className = 'ce--comment-time'
+
+    const commentContent = document.createElement('div')
+
+    const userName = document.createElement('span')
+    userName.innerHTML = commentData.user.username
+    userName.className = 'ce--comment-username'
+
+    const bodyEl = document.createElement('span')
+    bodyEl.innerHTML = commentData.body
+    bodyEl.className = 'ce--comment-text'
+
+    commentContent.className = 'ce--comment-content'
+    commentContent.append(userName)
+    commentContent.append(bodyEl)
+    commentContainer.append(timeEl)
+    commentContainer.append(commentContent)
+
+    return commentContainer
   }
 
-  function removeComments(commentsList) {
-    clearInterval(checkForCommentsInterval)
+  function attachComments(comments) {
+    const parentContainer = document.querySelector('.listenDetails')
+    const newCommentsContainer = document.createElement('div')
+    newCommentsContainer.className = 'ce--comments'
 
-    const newListContainer = document.createElement('div')
-    // ce--comments-list will be the new lists' parent
-    // adding the loading class renders the loading spinner
-    newListContainer.className = 'ce--comments-list loading'
-    commentsList.replaceWith(newListContainer)
-
-    fetchComments()
-  }
-
-  function checkForComments() {
-    const commentsList = document.querySelector('.commentsList').getElementsByTagName('ul')[0]
-    if (commentsList) {
-      removeComments(commentsList)
+    if (!comments.length) {
+      // const noComments = document.createElement('div')
+      // noComments.className = 'ce--no-comments'
+      // noComments.innerHTML = 'No comments yet'
+      // newCommentsContainer.append(noComments)
+      // parentContainer.append(newCommentsContainer)
+      return
     }
+
+
+    const header = document.createElement('div')
+    header.className = 'ce--header'
+    header.innerHTML = `${comments.length} comment${comments.length !== 1 ? 's' : ''}`
+    newCommentsContainer.append(header)
+
+    const listOfCommentsEl = document.createElement('ul')
+    listOfCommentsEl.className = 'ce--list'
+
+    // After header is created, map over comments and create html element for each
+    // Append all to newCommentsContainer, then append to body
+    comments
+      .sort((a, b) => a.timestamp - b.timestamp)
+      .map(createComment)
+      .forEach((comment) => listOfCommentsEl.append(comment))
+
+    newCommentsContainer.append(listOfCommentsEl)
+    parentContainer.append(newCommentsContainer)
+  }
+
+  function fetchComments(webUrl) {
+    const urlParts = webUrl.split('/')
+    const user = urlParts[3]
+    const track = urlParts[4]
+    const songPath = `/users/${user}/tracks/${track}`
+
+    return new Promise((resolve, reject) => {
+      SC.get(songPath, (track = {}, err) => {
+        if(err || !track.id){
+          reject(err || 'No track id')
+          return
+        }
+
+        const trackID = track.id
+        const commentsPath = `/tracks/${trackID}/comments`
+
+        return SC.get(commentsPath, (comments = [], err) => {
+          if (err) {
+            console.log('sc.get', err);
+            reject(err)
+            return
+          }
+
+          resolve(comments)
+        })
+      })
+		})
   }
 
 
-  const checkForCommentsInterval = setInterval(checkForComments, 100)
+  fetchComments(url)
+    .then(attachComments)
+    .catch(err => console.error('err', err))
 }
